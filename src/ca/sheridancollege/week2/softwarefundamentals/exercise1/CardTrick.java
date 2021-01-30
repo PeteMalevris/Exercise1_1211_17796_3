2d14e2f6a9bbc9cd217d5719d026bc8ea3c494fb
vu-193 <77688986+vu-193@users.noreply.github.com>
2021-01-29 21:46:08 -0500

    Second commit - Exercise1
    Added a print statement to let the user know what card they picked.

--- src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
@@ -38,6 +38,9 @@ public class CardTrick {
         System.out.print("Pick a card suit between 0 and 3 (inclusive): ");
         int suit = sc.nextInt();
         
+        // Print the card to the user
+        System.out.println("The card you picked was: " + value + " of " + Card.SUITS[suit]);
+        
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
